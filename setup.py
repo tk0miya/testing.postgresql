7f9ec6f77e81bde0e29854cda417f5ea49adb245
tk0miya <i.tkomiya@gmail.com>
2013-12-05 11:16:26 +0900

    Support python 3.2

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ classifiers = [
     "Programming Language :: Python",
     "Programming Language :: Python :: 2.6",
     "Programming Language :: Python :: 2.7",
+    "Programming Language :: Python :: 3.2",
     "Programming Language :: Python :: 3.3",
     "Topic :: Database",
     "Topic :: Software Development",
