6b39eda22f14677b9787577f1696bf7180e4fc06
tk0miya <i.tkomiya@gmail.com>
2014-02-13 15:27:16 +0900

    Install misspellings to .drone.io.sh

--- .drone.io.sh
+++ .drone.io.sh
@@ -2,6 +2,6 @@
 sudo apt-get update
 sudo apt-get install python2.6 python2.6-dev
 
-pip install --use-mirrors --upgrade detox
+pip install --use-mirrors --upgrade detox misspellings
 find src/ -name "*.py" | misspellings -f -
 detox
