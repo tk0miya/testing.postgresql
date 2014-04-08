ce6c5d5e973a1acd95eb81625937fa2247c29de8
tk0miya <i.tkomiya@gmail.com>
2014-04-08 17:13:16 +0900

    Run tests in py34

--- .drone.io.sh
+++ .drone.io.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 sudo apt-get update
-sudo apt-get install python2.6 python2.6-dev
+sudo apt-get install python2.6 python2.6-dev python3.4 python3.4-dev
 
 pip install --use-mirrors --upgrade detox misspellings
 find src/ -name "*.py" | misspellings -f -
