3e16052382968329128cbe694e7e7c7d32b0d41c
tk0miya <i.tkomiya@gmail.com>
2014-06-11 15:24:49 +0900

    Define release subcommand to setup.py

--- .drone.io.sh
+++ .drone.io.sh
@@ -2,6 +2,6 @@
 sudo apt-get update
 sudo apt-get install python2.6 python2.6-dev python3.4 python3.4-dev
 
-pip install --use-mirrors --upgrade detox misspellings
+pip install --use-mirrors --upgrade detox misspellings docutils
 find src/ -name "*.py" | misspellings -f -
 detox
