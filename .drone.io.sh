c16473e104c1a6dd215d6356da57af3f9c3b36f5
tk0miya <i.tkomiya@gmail.com>
2015-01-18 16:50:16 +0900

    Fix syntax error in .drone.io.sh

--- .drone.io.sh
+++ .drone.io.sh
@@ -3,6 +3,6 @@ sudo apt-get update
 sudo apt-get install python2.6 python2.6-dev python3.4 python3.4-dev
 
 pip install --use-mirrors --upgrade wheel
-pip install --use-mirrors --upgrade --use-wheel detox misspellings docutils eventlet<0.16
+pip install --use-mirrors --upgrade --use-wheel detox misspellings docutils "eventlet<0.16"
 find src/ -name "*.py" | misspellings -f -
 detox
