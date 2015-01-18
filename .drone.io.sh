96eab151e3e3fab33586e671c9069f93a5f6ee97
tk0miya <i.tkomiya@gmail.com>
2015-01-18 16:06:28 +0900

    Use wheel package at drone.io

--- .drone.io.sh
+++ .drone.io.sh
@@ -2,6 +2,7 @@
 sudo apt-get update
 sudo apt-get install python2.6 python2.6-dev python3.4 python3.4-dev
 
-pip install --use-mirrors --upgrade detox misspellings docutils
+pip install --use-mirrors --upgrade wheel
+pip install --use-mirrors --upgrade --use-wheel detox misspellings docutils
 find src/ -name "*.py" | misspellings -f -
 detox
