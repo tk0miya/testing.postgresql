dfe618736f5a126af8d7b0500d2a124466fa77fe
tk0miya <i.tkomiya@gmail.com>
2015-05-13 21:23:37 +0900

    Use new detox package on PyPI (now released!)

--- .drone.io.sh
+++ .drone.io.sh
@@ -3,7 +3,6 @@ sudo apt-get update
 sudo apt-get install python2.6 python2.6-dev python3.4 python3.4-dev
 
 pip install --use-mirrors --upgrade wheel
-pip install --use-mirrors --upgrade --use-wheel detox misspellings docutils "tox < 2.0"
-pip install https://bitbucket.org/hpk42/detox/get/tip.zip
+pip install --use-mirrors --upgrade --use-wheel detox misspellings docutils
 find src/ -name "*.py" | misspellings -f -
 detox
