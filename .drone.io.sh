7e1186c619c4e74386aa8e4f07601d797cdf74a6
tk0miya <i.tkomiya@gmail.com>
2015-05-13 12:49:14 +0900

    Use unreleased detox for testing

--- .drone.io.sh
+++ .drone.io.sh
@@ -3,6 +3,7 @@ sudo apt-get update
 sudo apt-get install python2.6 python2.6-dev python3.4 python3.4-dev
 
 pip install --use-mirrors --upgrade wheel
-pip install --use-mirrors --upgrade --use-wheel detox misspellings docutils "eventlet<0.16"
+pip install --use-mirrors --upgrade --use-wheel detox misspellings docutils
+pip install https://bitbucket.org/hpk42/detox/get/tip.zip
 find src/ -name "*.py" | misspellings -f -
 detox
