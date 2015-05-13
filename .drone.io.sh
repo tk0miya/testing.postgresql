d39cd268d89ca6be0ad258214e0209b57e581abb
tk0miya <i.tkomiya@gmail.com>
2015-05-13 22:14:49 +0900

    should be upgrade pip and setuptools on drone.io

--- .drone.io.sh
+++ .drone.io.sh
@@ -2,7 +2,7 @@
 sudo apt-get update
 sudo apt-get install python2.6 python2.6-dev python3.4 python3.4-dev
 
-pip install --use-mirrors --upgrade wheel
+pip install --use-mirrors --upgrade pip setuptools wheel
 pip install --use-mirrors --upgrade --use-wheel detox misspellings docutils
 find src/ -name "*.py" | misspellings -f -
 detox
