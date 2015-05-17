5d88cb71ad7fb883cc70eda8a9a1ec053b198fd8
Takeshi KOMIYA <i.tkomiya@gmail.com>
2015-05-17 13:25:53 +0900

    Use Travis CI as CI env instead of drone.io

--- .drone.io.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-sudo apt-get update
-sudo apt-get install python2.6 python2.6-dev python3.4 python3.4-dev
-
-pip install --use-mirrors --upgrade pip setuptools wheel
-pip install --use-mirrors --upgrade --use-wheel detox misspellings docutils
-find src/ -name "*.py" | misspellings -f -
-detox
